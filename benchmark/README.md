70a309f5ecc0f7c2402e41a9a4ee512b0aad026a
Matt Hinchliffe <matt@maketea.co.uk>
2018-04-16 08:58:27 +0100

    Add readme to benchmark app and cleanup results output

--- benchmark/README.md
+++ benchmark/README.md
@@ -1,1 +1,23 @@
 # Server-side rendering benchmark
+
+This is an application used to benchmark various JSX compatible rendering engines. It comprises of a Node.js Web server based upon [Fastify][1] and a script using [Apache Bench][2] to load test and record results.
+
+[1]: https://github.com/fastify/fastify
+[2]: https://httpd.apache.org/docs/2.4/programs/ab.html
+
+## Setup
+
+```sh
+# install application depdendencies
+$ npm install
+
+# run benchmark
+$ sh benchmark.sh
+```
+
+## Other tools
+
+To help diagnose performance bottlenecks it is recommended to use [Node Clinic][3] and/or [Flamebearer][4].
+
+[3]: https://github.com/nearform/node-clinic
+[4]: https://github.com/mapbox/flamebearer
--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,32 +4,36 @@ count=10000
 concurrency=20
 server=http://127.0.0.1:3000
 
-npm start & echo $! > app.pid; sleep 5 ;
+export NODE_ENV=production;
+
+npm start & echo $! > app.pid; sleep 5;
 
 date > results.txt
 
-echo "hyperapp" >> results.txt ;
-ab -k -n "$count" -c "$concurrency" $server/hyperapp | grep "Requests per second:" >> results.txt ;
-sleep 5 ;
+echo "\nHyperapp" >> results.txt;
+ab -k -n "$count" -c "$concurrency" $server/hyperapp | grep "Requests per second:" >> results.txt;
+sleep 5;
 
-echo "hyperons" >> results.txt ;
-ab -k -n "$count" -c "$concurrency" $server/hyperons | grep "Requests per second:" >> results.txt ;
-sleep 5 ;
+echo "\nHyperons" >> results.txt;
+ab -k -n "$count" -c "$concurrency" $server/hyperons | grep "Requests per second:" >> results.txt;
+sleep 5;
 
-echo "nerv" >> results.txt ;
-ab -k -n "$count" -c "$concurrency" $server/nerv | grep "Requests per second:" >> results.txt ;
-sleep 5 ;
+echo "\nNerv" >> results.txt;
+ab -k -n "$count" -c "$concurrency" $server/nerv | grep "Requests per second:" >> results.txt;
+sleep 5;
 
-echo "preact" >> results.txt ;
-ab -k -n "$count" -c "$concurrency" $server/preact | grep "Requests per second:" >> results.txt ;
-sleep 5 ;
+echo "\nPreact" >> results.txt;
+ab -k -n "$count" -c "$concurrency" $server/preact | grep "Requests per second:" >> results.txt;
+sleep 5;
 
-echo "rax" >> results.txt ;
-ab -k -n "$count" -c "$concurrency" $server/rax | grep "Requests per second:" >> results.txt ;
-sleep 5 ;
+echo "\nRax" >> results.txt;
+ab -k -n "$count" -c "$concurrency" $server/rax | grep "Requests per second:" >> results.txt;
+sleep 5;
 
-echo "react" >> results.txt ;
-ab -k -n "$count" -c "$concurrency" $server/react | grep "Requests per second:" >> results.txt ;
-sleep 5 ;
+echo "\nReact" >> results.txt;
+ab -k -n "$count" -c "$concurrency" $server/react | grep "Requests per second:" >> results.txt;
+sleep 5;
 
 kill $(cat app.pid) && rm app.pid
+
+unset NODE_ENV;
