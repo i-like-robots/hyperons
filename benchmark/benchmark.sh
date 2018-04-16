fcde0c25ec35944b12dfd0be1c338c4375f60713
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-04-16 11:41:59 +0100

    DRY out benchmarking script

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,39 +1,23 @@
 #!/bin/sh
 
+declare -a endpoints=("hyperapp" "hyperons" "nerv" "preact" "rax" "react")
+
 count=10000
 concurrency=20
 server=http://127.0.0.1:3000
 
 export NODE_ENV=production;
 
-npm start & echo $! > app.pid; sleep 5;
+npm start & echo $! > benchmark.pid; sleep 5;
 
 date > results.txt
 
-echo "\nHyperapp" >> results.txt;
-ab -k -n "$count" -c "$concurrency" $server/hyperapp | grep "Requests per second:" >> results.txt;
-sleep 5;
+for endpoint in "${endpoints[@]}"; do
+  echo "${endpoint}" >> results.txt;
+  ab -k -n "$count" -c "$concurrency" $server/$endpoint | grep "Requests per second:" >> results.txt;
+  sleep 5;
+done
 
-echo "\nHyperons" >> results.txt;
-ab -k -n "$count" -c "$concurrency" $server/hyperons | grep "Requests per second:" >> results.txt;
-sleep 5;
-
-echo "\nNerv" >> results.txt;
-ab -k -n "$count" -c "$concurrency" $server/nerv | grep "Requests per second:" >> results.txt;
-sleep 5;
-
-echo "\nPreact" >> results.txt;
-ab -k -n "$count" -c "$concurrency" $server/preact | grep "Requests per second:" >> results.txt;
-sleep 5;
-
-echo "\nRax" >> results.txt;
-ab -k -n "$count" -c "$concurrency" $server/rax | grep "Requests per second:" >> results.txt;
-sleep 5;
-
-echo "\nReact" >> results.txt;
-ab -k -n "$count" -c "$concurrency" $server/react | grep "Requests per second:" >> results.txt;
-sleep 5;
-
-kill $(cat app.pid) && rm app.pid
+kill $(cat benchmark.pid) && rm benchmark.pid
 
 unset NODE_ENV;
