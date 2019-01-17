68fd8635f3bf9f3c6df26b4f01bf05e820b85069
Matt Hinchliffe <matt@maketea.co.uk>
2019-01-17 08:24:16 +0000

    Skip flattening array of child elements as the render to string function supports nested arrays

--- src/create-element.js
+++ src/create-element.js
@@ -1,21 +1,7 @@
-function flattenArray(array, result = []) {
-  for (let i = 0, len = array.length; i < len; i++) {
-    const value = array[i]
-
-    if (Array.isArray(value)) {
-      flattenArray(value, result)
-    } else {
-      result.push(value)
-    }
-  }
-
-  return result
-}
-
 function createElement(type, props, ...children) {
   props = props || {}
 
-  props.children = flattenArray(children)
+  props.children = children || []
 
   // TODO: support default props
   return { type, props }
