a85a6f4327c024a85244ab89a756e5fb5ca75011
Matt Hinchliffe <matt@maketea.co.uk>
2022-03-17 18:51:35 +0000

    Update benchmarks

--- benchmark/engines/inferno.js
+++ benchmark/engines/inferno.js
@@ -1,5 +1,5 @@
 const { createElement } = require('inferno-create-element')
-const { renderToString } = require('inferno-server');
+const { renderToString } = require('inferno-server')
 const factory = require('./shared/factory')
 
 module.exports = () => renderToString(factory(createElement))
--- benchmark/engines/preact.js
+++ benchmark/engines/preact.js
@@ -1,5 +1,5 @@
 const { h } = require('preact')
-const renderToString = require('preact-render-to-string');
+const renderToString = require('preact-render-to-string')
 const factory = require('./shared/factory')
 
 module.exports = () => renderToString(factory(h))
